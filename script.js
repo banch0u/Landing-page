bc69804f270b44dc9cfa3ab417b7aa5ce8c8b863
banch0u <fikret.okis@gmail.com>
2022-06-09 14:17:05 +0400

    half decent working slider yay!

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ let slider2 = document.querySelector('#slider2');
 const slider = [slider0, slider1, slider2];
 
 rightArrowKey.addEventListener('click', function () {
-  slider[0].style = "transform: translateX(110%);"
-  slider[1].style = "transform: translateX(110%);"
-  slider[2].style = "transform: translateX(-220%);"
+  slider[0].style = "transform: translateX(119%);z-index: 80;"
+  slider[1].style = "transform: translateX(230%);z-index: 50;"
+  slider[2].style = "transform: translateX(6%);z-index: 30;"
   let tempSlide = slider.pop();
   slider.unshift(tempSlide);
 });
